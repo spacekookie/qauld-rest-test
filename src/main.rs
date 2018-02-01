ec4b3873f35d8108fcafc1f14649568774455c0a
Katharina Fey <kookie@spacekookie.de>
2018-02-01 05:00:09 +0100

    Adding a small example that uses qaul lib

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@ extern crate libqaul;
 
 use libqaul::*;
 use std::ffi::CString;
-use std::mem;
+
 
 fn main() {
 
     unsafe {
-        let q: *mut *mut qaul::qaul = mem::uninitialized();
-        ql_initialise(q, qaul_os_LINUX, CString::new("path").unwrap().as_ptr(), CString::new("path").unwrap().as_ptr());
+        let mut qaul: *mut qaul::qaul = std::ptr::null_mut();
+        let status = ql_initialise(&mut qaul, qaul_os_LINUX, CString::new("path").unwrap().as_ptr(), CString::new("path").unwrap().as_ptr());
+
+        match status {
+            ql_error_t_SUCCESS => println!("Initialisation was a success!"),
+            _ => {}
+        }
+
+        let status2 = ql_shutdown(qaul);
+        match status2 {
+            ql_error_t_NOT_INITIALISED => println!("Shutdown didn't do anything!!"),
+            _ => {}
+        }
     }
 
     println!("Hello, world!");
